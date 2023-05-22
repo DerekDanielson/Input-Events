e2e283541c370434c0f48b50516d2868b0d0c6fe
Derek <derekdanielson84@gmail.com>
2023-05-22 08:58:06 -0600

    Notes added

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ const priorityInput = document.getElementById('priority-input');
 const checkbox = document.getElementById('checkbox');
 const heading = document.querySelector('h1');
 
+
+function onInput(e){
+    heading.textContent = e.target.value;
+}
+
+itemInput.addEventListener('input', onInput);
