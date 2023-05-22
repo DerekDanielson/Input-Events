ad2f85a4334434c81bb8fe9641cd6e19653d6fd7
Derek <derekdanielson84@gmail.com>
2023-05-22 09:10:36 -0600

    Notes added

--- app.js
+++ app.js
@@ -8,4 +8,26 @@ function onInput(e){
     heading.textContent = e.target.value;
 }
 
+function onChecked(e){
+    const isChecked = e.target.checked;
+    heading.textContent = isChecked ? 'Checked' : 'Not Checked';
+}
+
+function onFocus(e){
+    console.log('Input is focused');
+    itemInput.style.outlineStyle = 'solid';
+    itemInput.style.outlineWidth = '2px';
+    itemInput.style.outlineColor = 'green';
+}
+
+function onBlur(e){
+    console.log('Input is not focused');
+    itemInput.style.outlineStyle = 'none';
+}
+
+
 itemInput.addEventListener('input', onInput);
+priorityInput.addEventListener('input', onInput);
+checkbox.addEventListener('input', onChecked);
+itemInput.addEventListener('focus', onFocus);
+itemInput.addEventListener('blur', onBlur);
